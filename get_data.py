5799e5acd203cb38c1ab2fb08ca9383e15469b60
Pierro13 <Idr0neb2@gmail.com>
2022-12-09 16:04:12 +0100

    MAJ getdata

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 from urllib import request
 url = 'https://ressources.data.sncf.com/explore/dataset/regularite-mensuelle-tgv-aqst/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B'
 fichier = 'data.csv'
-url2 = 'https://data.sncf.com/explore/dataset/liste-des-gares/download/?format=geojson&refine.voyageurs=O&refine.fret=N&timezone=Europe/Paris&lang=fr'
-fichier2 = 'data.geojson'
+url2 = 'https://data.sncf.com/explore/dataset/liste-des-gares/download/?format=geojson&timezone=Europe/Paris&lang=fr'
+fichier2 = 'coords.geojson'
 request.urlretrieve(url, fichier)
+request.urlretrieve(url2, fichier2)
