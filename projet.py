8c224a007ca0f1ccd8e6f992dae6d5b8295b3099
Pierro13 <Idr0neb2@gmail.com>
2022-12-21 15:41:15 +0100

    néttoyage fichiers en trop

--- projet.py
+++
@@ -1,7 +0,0 @@
-import folium, geopandas as gpd
-
-coords = (46.227638,2.213749)
-map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=6)
-data = gpd.read_file("Projet\regularite-mensuelle-tgv-aqst.csv")
-
-geo = data.to_json()
--- test_s_where.py
+++
@@ -1,5 +0,0 @@
-#launch python file called map_gares.py
-
-import map_gares
-
-map_gares.create_map()
